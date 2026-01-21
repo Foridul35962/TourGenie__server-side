8842c8f819f767e75e330a3848c819c8ff98db64
Foridul35962 <foridulislam35962@gmail.com>
2026-01-22 03:07:18 +0600

    swaggerUi add for api documentation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import cookies from 'cookie-parser'
+import swaggerUi from "swagger-ui-express";
+
+//local import
+import errorHandler from './helpers/ErrorHandler.js'
+import { swaggerSpec } from './config/swagger.js';
+import authRouter from './routes/auth.routes.js';
+import "./config/redis.js";
 
 const app = express()
 
@@ -12,10 +19,19 @@ app.use(cors({
 app.use(cookies())
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req,res)=>{
+//routes
+app.use('/api/auth', authRouter)
+
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+app.get('/', (req, res) => {
     res.send('TourGenie server is running ...')
 })
+
+app.use(errorHandler)
 
 export default app
